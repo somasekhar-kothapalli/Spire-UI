ffb3013f785421acc30eb5317c9217409532417e
Kothapalli Somasekhar <somunani2413@gmail.com>
2025-05-16 22:27:37 +0530

    feat(utils): add system utility functions to detect browser engine and OS
    This commit introduces two new utility functions: `getBrowserEngine` and `getOperatingSystemName`. These functions help identify the browser engine (e.g., Chromium, WebKit, Gecko) and the operating system (e.g., macOS, Windows, Linux) based on the user agent and platform information. This functionality is essential for platform-specific logic in the application.

---
+++ src/utils/system.js
@@ -0,0 +1,44 @@
+let os;
+let engine;
+
+// @type () => "chromium" || "webkit" || "gecko" || "other"
+export let getBrowserEngine = () => {
+  if (engine === undefined) {
+    if (navigator.userAgent.indexOf("Firefox/") > -1) {
+      engine = "gecko";
+    } else if (navigator.userAgent.indexOf("Chrome") > -1) {
+      engine = "chromium";
+    } else if (navigator.userAgent.indexOf("Safari/") > -1) {
+      engine = "webkit";
+    } else {
+      engine = "other";
+    }
+  }
+
+  return engine;
+};
+
+// @type () => "macos" || "windows" || "linux" || "chromeos" || "android" || "ios" || null
+export let getOperatingSystemName = () => {
+  if (os === undefined) {
+    if (navigator.platform.startsWith("Mac") === true) {
+      os = "macos";
+    } else if (["iPhone", "iPad", "iPod"].indexOf(navigator.platform) !== -1) {
+      os = "ios";
+    } else if (
+      ["Win32", "Win64", "Windows", "WinCE"].indexOf(navigator.platform) !== -1
+    ) {
+      os = "windows";
+    } else if (/CrOS/.test(navigator.userAgent)) {
+      os = "chromeos";
+    } else if (/Android/.test(navigator.userAgent)) {
+      os = "android";
+    } else if (/Linux/.test(navigator.platform)) {
+      os = "linux";
+    } else {
+      os = null;
+    }
+  }
+
+  return os;
+};
